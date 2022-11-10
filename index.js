005d730ea921c2f98e30eefe7a8c530146d1903e
ayitzhak <aviram.tbj@gmail.com>
2022-11-11 01:50:31 +0200

    fetch data and show basic details

--- index.js
+++ index.js
@@ -8,12 +8,36 @@
 // hint: use this reference later to inject data into your page
 const app = document.getElementById('app');
 
-async function getData() {
-  // write you logic for getting the data from the API here
-  // return your data from this function
-  const data = null;
+//////// DATA functions and consts
+// Endpoint URLs consts
+const baseURL = "https://www.balldontlie.io/api/v1/";
+const allTeamsSuffix = "/teams";
+const allPlayersSuffix = "/players"
+
+async function queryEndpoint(endpoint) {
+  let data = null;
+  try {
+    const response = await fetch(endpoint);
+    console.log(response);
+    if (response.status === 200) {
+      data = await response.json();
+    }
+  } catch (error) {
+    console.log('api error');
+    console.error(error);
+  }
   return data;
 }
+
+async function getAllTeams() {
+  return (await queryEndpoint(baseURL.concat(allTeamsSuffix))).data;
+}
+
+async function getAllPlayers() {
+  return await queryEndpoint(baseURL.concat(allPlayersSuffix)).data;
+}
+
+//////// END DATA
 
 function clearUI() {
   while (app.firstChild) {
@@ -21,19 +45,31 @@ function clearUI() {
   }
 }
 
-async function renderUI(data) {
+function populateContentElementWithData(contentElement, dataElement) {
+
+  contentElement.innerHTML = `<h2>${dataElement.full_name}</h2><p>${dataElement.city}</p>`
+
+}
+
+async function renderUI() {
 
   clearUI();
 
   // you have your data! add logic here to render it to the UI
   // notice in the HTML file we call render();
-  const dummyItemElement = Object.assign(document.createElement("div"), { className: "item" })
-  const dummyContentElement = Object.assign(document.createElement("div"), { className: "content" })
-  dummyContentElement.innerHTML = "hey";
-  dummyItemElement.appendChild(dummyContentElement);
-  app.appendChild(dummyItemElement);
+
+  allTeamsData.forEach(teamElement => {
+    const dummyItemElement = Object.assign(document.createElement("div"), { className: "item" })
+    const dummyContentElement = Object.assign(document.createElement("div"), { className: "content" })
+    populateContentElementWithData(dummyContentElement, teamElement);
+    dummyItemElement.appendChild(dummyContentElement);
+    app.appendChild(dummyItemElement);
+  }); {
+  }
 }
 
-const data = await getData();
+const allTeamsData = await getAllTeams();
+console.log(allTeamsData);
+const allPlayersData = await getAllPlayers();
 
-await renderUI(data);
+await renderUI();
